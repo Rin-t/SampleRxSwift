8a931e91a6dc882f5c057ac07c40c3480feeb43d
Rin <rin.t.4921@gmail.com>
2022-05-16 23:55:56 +0900

    非同期を再現

--- SampleRxSwift/CalculationRepository.swift
+++ SampleRxSwift/CalculationRepository.swift
@@ -10,6 +10,18 @@ import RxSwift
 
 struct CalculationRepository: CalculationRepositoryProtocol {
     func calculate(num1: Int, num2: Int) -> Single<Int> {
+//        return Single.just(num1 + num2)
+//
+//        return Single.create { single in
+//
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 5, execute: {
+//                single(.success(num1 + num2))
+//            })
+//
+//            return Disposables.create()
+//        }
+
         return Single.just(num1 + num2)
+            .delay(.seconds(5), scheduler: MainScheduler.instance)
     }
 }
